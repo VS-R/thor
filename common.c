d96fd0a9eec9cc080910929124042b22b2518faf
anaximeno <anaximenobrito@gmail.com>
2021-01-21 00:24:55 -0100

    I maked the inicial layout

---
+++ common.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/*                       docstrings
+ * Este módulo esta a ser utilizado para guardar funções que
+ * serão utilizadas no modulo principal main.c
+ *
+ * No que se refere a explicações dos códicos,
+ * a maioria encontra-se aqui abaixo, pelo que,
+ * só comentários realmente necessários serão
+ * adicionados aos códicos. Também foram usados
+ * nomes bastantes sugestivos o que facilita o
+ * entendimento do processo.
+ * 
+ * Funções principais:
+ * TODO: coloca funções principais ali
+ * */
+
+
---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*                    docstring
+ * O módulo common.c contém as funções que realizam a
+ * maioria dos processos deste módulo.
+ * */
+
+#include "common.c"
+
+
+int main()
+{
+	return 0;
+}
