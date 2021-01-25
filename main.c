330bc961267969499061bc777190bc251ad0ca8b
anaximeno <anaximenobrito@gmail.com>
2021-01-24 23:03:29 -0100

    statiscs part added

--- main.c
+++ main.c
@@ -1,11 +1,11 @@
+/*                    docstrings
+* O módulo common.c contém as funções que realizam a
+* maioria dos processos deste módulo.
+* */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-/*                    docstrings
- * O módulo common.c contém as funções que realizam a
- * maioria dos processos deste módulo.
- * */
 
 #include "common.c"
 
@@ -14,20 +14,17 @@ int main()
 {
 	struct setores setor[STRUCTMAX];
 	setor->counter = 0;
-	setor->consumo = 0;
+	setor->consumo_eletrico = 0;
+	setor->escudos_consumidos = 0;
 	setor->aparelho->counter = 0;
+	setor->aparelho->escudos_consumidos = 0;
 
 	int index, again, i;
-
-	/* TODO: verify this;
-	int tarifa_Residencial_electra;
-
-	printf("Escreva o valor da Tarifa Residencial Cobrado pela ELECTRA: ");
-	scanf("%lf", &tarifa_Residencial_electra); */
+	float valor_unitario, escudos_consumidos, consumo_e_taxas, total_a_pagar;
 
 	printf("\n# SEJA BEMVINDO(A) ao Simulador de Consumo de Energia: TH0R\n");
 	while(1){
-		switch (menu()) {
+		switch(menu()) {
 			case 1:
 				add_setor(setor);
 				break;
@@ -38,7 +35,7 @@ int main()
 					setbuf(stdin, NULL);
 
 					if (setor->counter > 0 && index >= 0 && index < setor->counter){	
-						do{
+						do {
 							printf("\n\t* %s *\n", setor[index].nome);
 							add_aparelho(setor, index);
 
@@ -53,9 +50,17 @@ int main()
 					printf("Nenhum setor disponível! Adiciona setores na opção 1 e depois volta para cá.\n");
 				break;
 			case 3:
-				// TODO: show stats and out exit!
-				// TODO: mostrar comsumo por setor em ESCUDOS CVE
-				// printf("O consumo do primeiro setor %s é %fkwh\n", setor[0].nome, setor[0].consumo);
+				printf("Escreva o valor unitário atual da Tarifa Residêncial de Baixa Tensão: ");
+				scanf("%f", &valor_unitario);
+
+				escudos_consumidos = calcular_escudos_consumidos(setor, valor_unitario);
+				consumo_e_taxas = escudos_consumidos + TAXA_RTC + IP_CONTRIB + ALUGER;
+				total_a_pagar =  consumo_e_taxas + (consumo_e_taxas * IVA);
+
+				for(i = 0 ; i < setor->counter ; ++i)
+					show_dados_setor(setor, i);
+
+				printf("\n** O valor total de escudos a pagar é %.2fCVE\n", total_a_pagar);
 				exit(0);
 				break;
 			default:
